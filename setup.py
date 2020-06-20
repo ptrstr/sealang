998f373ab364ac53f44276fcf797d7a47c5c155c
Andrey Torsunov <andrey.torsunov@gmail.com>
2020-06-20 22:59:43 +0300

    Remove garbage from setup.py

--- setup.py
+++ setup.py
@@ -1,83 +1,82 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import setup, Extension
+# coding: utf-8
 
-import os
-import platform
-import subprocess
 import sys
-import textwrap
 
-# Set up the build environment.
-llvm_home = os.environ.get('LLVM_HOME', '/usr')
-llvm_config = os.path.join(llvm_home, 'bin', 'llvm-config')
-
-if not os.path.isfile(llvm_config):
-    print("Unable to set up build environment. Have you installed LLVM and set LLVM_HOME?")
-
-    def dedent_print(s):
-        print(textwrap.dedent(s))
-
-    if platform.system().lower() == 'darwin':
-        dedent_print("""
-        Using homebrew:
-            brew install llvm --with-clang --with-asan
-            export LLVM_HOME=/usr/local/opt/llvm
-            export DYLD_LIBRARY_PATH=$LLVM_HOME/lib
-        """)
-    elif platform.dist()[0].lower() == 'ubuntu':
-        dedent_print("""
-        Using apt-get:
-            sudo apt-get install libclang-10.0 clang-10.0 -y
-            export LLVM_HOME=/usr/lib/llvm-10.0
-            export LD_LIBRARY_PATH=$LLVM_HOME/lib
-        """)
-    elif platform.dist()[0].lower() == 'arch':
-        dedent_print("""
-        Using pacman:
-            sudo pacman -S llvm clang"
-        """)
+major, minor = sys.version_info[:2]
+if major < 3 or minor < 7:
+    print("Unsupported Python version. Version requirement is >= 3.7")
     sys.exit(1)
 
-llvm_cflags = subprocess.check_output([llvm_config, '--cxxflags']).decode('utf-8').split()
-llvm_ldflags = subprocess.check_output([llvm_config, '--ldflags']).decode('utf-8').split()
+import os
+import subprocess
 
+from setuptools import setup, Extension
+from pathlib import Path
+
+llvm_home = os.environ.get("LLVM_HOME", "/usr")
+
+
+def iter_llvm_config():
+    paths = (
+        Path(llvm_home, "bin", "llvm-config-10"),
+        Path(llvm_home, "bin", "llvm-config"),
+        Path("/usr/bin/llvm-config-10"),
+        Path("/usr/bin/llvm-config"),
+    )
+
+    for p in paths:
+        print(f"Check {p}. is_file = {p.is_file()}, is_symlink = {p.is_symlink()}")
+        if p.is_file() or p.is_symlink():
+            yield p
+
+
+llvm_config = next(iter_llvm_config(), None)
+if llvm_config is None:
+    print(
+        "Unable to set up build environment. Have you installed LLVM and set LLVM_HOME?"
+    )
+    sys.exit(1)
+
+llvm_cflags = (
+    subprocess.check_output([str(llvm_config), "--cxxflags"]).decode("utf-8").split()
+)
+llvm_ldflags = (
+    subprocess.check_output([str(llvm_config), "--ldflags"]).decode("utf-8").split()
+)
 
 setup(
-    name='sealang',
-    version='10.0',
-    description='An extended set of Python bindings for libclang (fork of pybee/sealang)',
-    long_description=open('README.rst').read(),
-    url='http://github.com/gtors/sealang',
-    license='License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+    name="sealang",
+    version="10.0",
+    description="An extended set of Python bindings for libclang (fork of pybee/sealang)",
+    long_description=open("README.rst").read(),
+    url="http://github.com/gtors/sealang",
+    license="License :: OSI Approved :: University of Illinois/NCSA Open Source License",
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
-        'Programming Language :: Python',
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Software Development :: Compilers',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: University of Illinois/NCSA Open Source License",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Compilers",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords=['llvm', 'clang', 'libclang'],
-    author='LLVM team, Russell Keith-Magee and Andrey Torsunov',
-    author_email='andrey.torsunov@gmail.com',
-    packages=[
-        'clang',
-        'sealang'
-    ],
+    keywords=["llvm", "clang", "libclang"],
+    author="LLVM team, Russell Keith-Magee and Andrey Torsunov",
+    author_email="andrey.torsunov@gmail.com",
+    packages=["clang", "sealang"],
     ext_modules=[
         Extension(
-            'sealang',
-            sources=['sealang/sealang.cpp'],
-            libraries=['clang-cpp'],
+            "sealang",
+            sources=["sealang/sealang.cpp"],
+            libraries=["clang-cpp"],
             extra_compile_args=llvm_cflags,
             extra_link_args=llvm_ldflags,
         ),
     ],
     # if nose.collector is used, many plugins will not be available
     # see: https://nose.readthedocs.io/en/latest/setuptools_integration.html
-    test_suite='nose.collector',
-    tests_require=['nose'],
+    test_suite="nose.collector",
+    tests_require=["nose"],
     zip_safe=True,
 )
