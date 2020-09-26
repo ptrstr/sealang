23a404be1db14907a11d623f6a12dd04fae15f2b
ptrstr <ptrstr@protonmail.com>
2020-09-26 11:07:41 -0400

    Fixed function exporting issue

--- sealang/sealang.h
+++ sealang/sealang.h
@@ -7,6 +7,12 @@
 #include "clang-c/Index.h"
 #include "clang-c/CXString.h"
 
+#ifdef _WIN32
+#define EXPORT_PREFIX __declspec(dllexport)
+#else
+#define EXPORT_PREFIX
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -14,46 +20,46 @@ extern "C" {
 /**
  * \brief Returns string representation of unary and binary operators
  */
-CXString clang_Cursor_getOperatorString(CXCursor cursor);
+EXPORT_PREFIX CXString clang_Cursor_getOperatorString(CXCursor cursor);
 
 /**
  * \brief Returns Opcode of binary operator
  */
-clang::BinaryOperatorKind clang_Cursor_getBinaryOpcode(CXCursor cursor);
+EXPORT_PREFIX clang::BinaryOperatorKind clang_Cursor_getBinaryOpcode(CXCursor cursor);
 
 /**
  * \brief Returns Opcode of unary operator
  */
-clang::UnaryOperatorKind clang_Cursor_getUnaryOpcode(CXCursor cursor);
+EXPORT_PREFIX clang::UnaryOperatorKind clang_Cursor_getUnaryOpcode(CXCursor cursor);
 
 /**
  * \brief Returns string representation of literal cursor (1.f, 1000L, etc)
  */
-CXString clang_Cursor_getLiteralString(CXCursor cursor);
+EXPORT_PREFIX CXString clang_Cursor_getLiteralString(CXCursor cursor);
 
 /**
  * \brief Returns for-loop init cursor [for(init;cond;inc)], or CXCursor_NoDeclFound if there is no decl,
  * or CXCursor_InvalidCode if C is not CXCursor_ForStmt
  */
-// CXCursor clang_getForStmtInit(CXCursor C);
+// EXPORT_PREFIX CXCursor clang_getForStmtInit(CXCursor C);
 
 /**
  * \brief Returns for-loop condition cursor [for(init;cond;inc)], or CXCursor_NoDeclFound if there is no decl,
  * or CXCursor_InvalidCode if C is not CXCursor_ForStmt
  */
-// CXCursor clang_getForStmtCond(CXCursor C);
+// EXPORT_PREFIX CXCursor clang_getForStmtCond(CXCursor C);
 
 /**
  * \brief Returns for-loop increment cursor [for(init;cond;inc)], or CXCursor_NoDeclFound if there is no decl,
  * or CXCursor_InvalidCode if C is not CXCursor_ForStmt
  */
-// CXCursor clang_getForStmtInc(CXCursor C);
+// EXPORT_PREFIXCXCursor clang_getForStmtInc(CXCursor C);
 
 /**
  * \brief Returns for-loop body, or CXCursor_NoDeclFound if there is no decl,
  * or CXCursor_InvalidCode if C is not CXCursor_ForStmt
  */
-// CXCursor clang_getForStmtBody(CXCursor C);
+// EXPORT_PREFIX CXCursor clang_getForStmtBody(CXCursor C);
 
 
 #ifdef __cplusplus
